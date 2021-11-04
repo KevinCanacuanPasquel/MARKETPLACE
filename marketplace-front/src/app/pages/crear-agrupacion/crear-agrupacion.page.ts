405b9afb73aae14989f166494e59dd3297789c06
jukishio <jukishio@hotmail.com>
2021-11-04 18:39:22 -0500

    eliminar metodo repetido

--- marketplace-front/src/app/pages/crear-agrupacion/crear-agrupacion.page.ts
+++ marketplace-front/src/app/pages/crear-agrupacion/crear-agrupacion.page.ts
@@ -137,57 +137,7 @@ export class CrearAgrupaciones {
   
 
 
-  changeListener(e) : void {
-    //this.images=[];
-    //this.filesToLoad=[];
-    if( e.target.files ){
-      if ((e.target.files.length + this.images.length) <=3  ) {
-        let flag=true;
-        for (var i = 0; i < e.target.files.length; i++) {  
-          let file = e.target.files[i];
-          if( (file.size/1024.0)/1024.0 > 9  ){
-            flag=false;
-            this.images=[];
-            console.log("No puede cargar archivos de mas de 9MB")
-            //    this.presentToast("No puede cargar archivos de mas de 9MB","danger");
-            break;
-          }
-        }
-        if( flag ){
-          for (var i = 0; i < e.target.files.length; i++) {  
-              let reader = new FileReader();
-              let file = e.target.files[i];
-              //console.log("==>tamanio de archivo " + file.size )
-              reader.readAsDataURL(file);  
-              console.log("el archivooooo", file)            
-              reader.onload = () => {        
-                this.images.push( reader.result );  
-                let dataUpload:DataUpload = {
-                  name: file.name,
-                  ext: file.type,
-                  fecha: new Date(),
-                  fileBase64: String(reader.result).split(",")[1],
-                           
-                };
-                this.filesToLoad.push(dataUpload);  
-              }  
-            //console.log("===>seleccionados " + this.filesToLoad.length);
-          }
-        }
-          
-        
-      } else {
-        //this.images=[];
-        //this.filesToLoad=[];
-    //    this.presentToast("No puede cargar mas de 3 archivos","danger");
-      }
-    } else {
-      this.images=[];
-      this.filesToLoad=[];
-      console.log("esta entrando aqui")
-  //    this.presentToast("No selecciono archivos","danger");
-    }
-  }
+  
   verImagenes(){
    
     this.srcJoya = this.filesToLoad[0].fileBase64
