f59104dab63141981522446bc4c50930267b728f
jukishio <jukishio@hotmail.com>
2022-01-23 21:25:47 -0500

    backend agenda

--- marketplace-server/dist/routes/servicio copy.js
+++ marketplace-server/dist/routes/servicio copy.js
@@ -16,6 +16,8 @@ const express_1 = require("express");
 const autenticacion_1 = require("../middlewares/autenticacion");
 const file_system_1 = __importDefault(require("../classes/file-system"));
 const servicio_model_1 = require("../models/servicio.model");
+const actividad_model_1 = require("../models/actividad.model");
+const suscripcion_model_1 = require("../models/suscripcion.model");
 const servicioRoutes = (0, express_1.Router)();
 const fileSystem = new file_system_1.default();
 //AGRUPACION - Obtener agrupaciones paginadas
@@ -83,7 +85,7 @@ servicioRoutes.post('/crearServicio', [autenticacion_1.verificaToken], (req, res
 servicioRoutes.put('/actualizarServicio', (req, res) => {
     console.log("llega el servicio desde arriba", req.body);
     const servicio = req.body;
-    servicio._id = req.body.id;
+    servicio._id = req.body._id;
     console.log("llega el servicio", servicio);
     servicio_model_1.Servicio.findByIdAndUpdate(servicio._id, servicio, { new: true }, (err, servicioDB) => {
         if (err)
@@ -110,53 +112,60 @@ servicioRoutes.delete('/eliminarServicio', (req, res) => {
     })
         .catch(error => console.error(error));
 });
-//Servicio para subir archivos
-/*
-agrupacionRoutes.post('/cargarImagenesAgrupacion', [verificaToken],  async (req: Request, res: Response) => {
-
-    if ( !req.files ) {
-        return res.status(400).json({
-            ok: false,
-            mensaje: 'No se subio ningun archivo'
+//   BUSQUEDA -por parameetros
+servicioRoutes.get('/servicioByParametros', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("pruebas");
+    const nombre = req.query.actividad;
+    const arte = req.query.arte;
+    const estado = req.query.estado;
+    console.log(arte, nombre);
+    let actividades;
+    let listIdActividad;
+    let listIdAgrupaciones;
+    let fechaActual = new Date().toISOString();
+    let query = { $and: [{ fechaFin: { $gte: fechaActual } }, { fechaInicio: { $lt: fechaActual } }] };
+    const agrupaciones = yield suscripcion_model_1.Suscripcion.find(query).select('agrupacion')
+        .exec();
+    console.log("agrupaciones", agrupaciones);
+    listIdAgrupaciones = agrupaciones.map(x => { return x.agrupacion; });
+    console.log("agrupaciones lista id", listIdAgrupaciones);
+    if (arte && nombre) {
+        actividades = yield actividad_model_1.Actividad.find({ nombre: { $regex: nombre }, arte: { $regex: arte }, estado: estado }).select('_id').exec();
+        listIdActividad = actividades.map(x => { return x._id; });
+        const servicios = yield servicio_model_1.Servicio.find({ $and: [{ actividad: { $in: listIdActividad } }, { agrupacion: { $in: listIdAgrupaciones } }] });
+        //const servicios =  await Servicio.find(  {agrupacion: {$in: listIdAgrupaciones}})
+        //      console.log("servicios", servicios)
+        res.json({
+            ok: true,
+            servicios
         });
     }
-
-    const file: FileUpload = req.files.imagen;
-
-    if ( !file ){
-        return res.status(400).json({
-            ok: false,
-            mensaje: 'No se subio ningun archivo - imagen'
+    else if (nombre) {
+        actividades = yield actividad_model_1.Actividad.find({ nombre: { $regex: nombre }, estado: estado }).select('_id').exec();
+        listIdActividad = actividades.map(x => { return x._id; });
+        const servicios = yield servicio_model_1.Servicio.find({ $and: [{ actividad: { $in: listIdActividad } }, { agrupacion: { $in: listIdAgrupaciones } }] });
+        res.json({
+            ok: true,
+            servicios
         });
     }
-
-    if ( !file.mimetype.includes('image') ) {
-        return res.status(400).json({
-            ok: false,
-            mensaje: 'Lo que cargo no es una imagen'
+    else if (arte) {
+        actividades = yield actividad_model_1.Actividad.find({ arte: { $regex: arte }, estado: estado }).select('_id').exec();
+        listIdActividad = actividades.map(x => { return x._id; });
+        const servicios = yield servicio_model_1.Servicio.find({ $and: [{ actividad: { $in: listIdActividad } }, { agrupacion: { $in: listIdAgrupaciones } }] });
+        res.json({
+            ok: true,
+            servicios
         });
     }
-
-    await fileSystem.guardarImagenTemporal( file, req.usuario._id );
-    
-
-    res.json({
-        ok: true,
-        file: file.mimetype
-    });
-
-})
-
-
-//Mostrar Imagenes
-agrupacionRoutes.get('/imagen/:userid/:img', ( req: any, res: Response) => {
-
-    const userId = req.params.userid;
-    const img = req.params.img;
-
-    const pathFoto = fileSystem.getFotoUrl( userId, img);
-
-    res.sendFile(pathFoto);
-});
-*/
+    else {
+        actividades = yield actividad_model_1.Actividad.find({ estado: estado }).select('_id').exec();
+        listIdActividad = actividades.map(x => { return x._id; });
+        const servicios = yield servicio_model_1.Servicio.find({ $and: [{ actividad: { $in: listIdActividad } }, { agrupacion: { $in: listIdAgrupaciones } }] });
+        res.json({
+            ok: true,
+            servicios
+        });
+    }
+}));
 exports.default = servicioRoutes;
---
+++ marketplace-server/routes/agenda.ts
@@ -0,0 +1,217 @@
+import { Router, Request, Response } from "express";
+import { Usuario } from '../models/usuario.model';
+import bcrypt from 'bcrypt';
+import Token from '../classes/token';
+import { verificaToken } from '../middlewares/autenticacion';
+import { Agrupacion } from '../models/agrupacion.model';
+import { FileUpload } from '../interfaces/file-upload';
+import FileSystem from "../classes/file-system";
+import { Servicio } from "../models/servicio.model";
+import { Actividad } from "../models/actividad.model";
+import { Suscripcion } from "../models/suscripcion.model";
+import { Agenda } from "../models/agenda.model";
+
+
+const agendaRoutes = Router();
+const fileSystem = new FileSystem();
+
+
+//AGRUPACION - Obtener agrupaciones paginadas
+agendaRoutes.get('/agencia',  async (req:any, res:Response) => {
+
+    //Buscar por paginas
+    let pagina = Number(req.query.pagina) || 1;
+    let skip = pagina - 1;
+    skip = skip * 10;
+
+    const agenda = await Agenda.find()
+                                        .sort({ _id: -1 })
+                                        .skip( skip )
+                                        .limit(10)
+                                        .exec();
+
+    res.json({
+        ok: true,
+        pagina,
+        agenda
+    });
+});
+
+
+
+//AGRUPACIONES - Obtener servicio by id
+///
+agendaRoutes.get('/agendaById',  async (req:any, res:Response) => {
+    const id = req.query.id;
+    console.log(req.query.agrupId)
+    
+    const servicios = await Agenda.findById(id).populate('').populate('agrupacion')                    
+                                        .exec();
+
+    res.json({
+        ok: true,
+        servicios
+    });
+});
+
+//AGRUPACIONES - Obtener agrupaciones por usuario
+///
+agendaRoutes.get('/serviciosByCliente',  async (req:any, res:Response) => {
+    const clienteId = req.query.clienteId;
+    console.log(req.query.agrupId)
+    var query = {cliente : clienteId};
+    
+    const servicios = await Servicio.find(query).populate('servicio').populate('usuario')                      
+                                        .exec();
+
+    res.json({
+        ok: true,
+        servicios
+    });
+});
+
+
+
+
+//AGRUPACION - Crear
+agendaRoutes.post('/crearServicio', [verificaToken],  (req:any, res:Response) => {
+
+    let body = req.body;
+    body.servicio = req.body.servicio;
+    body.cliente =req.body.cliente;
+    console.log("el body", body)
+   // const imagenes = fileSystem.imagenesDeTempHaciaAgrupaciones( req.usuario._id );
+    //body.fotos = imagenes;
+
+    Agenda.create(body).then ( async agendaDB => {
+
+        await agendaDB.populate('servicio').populate('usuario').execPopulate();
+
+        res.json({
+            ok: true,
+            agenda: agendaDB
+        });
+
+    }).catch( err => {
+        res.json(err)
+    });
+
+
+});
+/*
+///ACTUALIZAR
+//USUARIO - Actualizar
+servicioRoutes.put('/actualizarServicio',   (req: any, res: Response) => {
+    console.log("llega el servicio desde arriba" ,  req.body)
+    const servicio =  req.body
+    servicio._id = req.body._id
+    console.log("llega el servicio" , servicio)
+    Servicio.findByIdAndUpdate( servicio._id, servicio, { new: true }, ( err, servicioDB ) => {
+
+        if ( err ) throw err;
+
+        if ( !servicioDB ) {
+            return res.json({
+                ok: false,
+                mensaje: 'No existe un servicio con ese ID'
+            });
+        }
+
+        //Token
+     
+            
+            res.json({
+                ok: true,
+                servicio: servicioDB
+            });
+
+    });
+
+});
+
+servicioRoutes.delete('/eliminarServicio', (req: any, res: Response) => {
+    
+    Servicio.deleteOne(
+      { _id: req.query.id }
+      
+    ).then(result => {
+        if (result.deletedCount === 0) {
+          return res.json('No se encontro el servicio')
+        }
+        res.json( "Se elmino el servicio")
+      })
+      .catch(error => console.error(error))
+
+  })
+
+
+
+
+//   BUSQUEDA -por parameetros
+servicioRoutes.get('/servicioByParametros', async (req:any, res:Response)=>{
+    console.log("pruebas")
+    const nombre = req.query.actividad;
+    const arte = req.query.arte;
+    const estado = req.query.estado
+    console.log(arte, nombre)
+    let actividades
+    let listIdActividad
+    let listIdAgrupaciones
+    let fechaActual = new Date().toISOString();
+    let query =  { $and: [ { fechaFin: {$gte: fechaActual} }, {fechaInicio :{$lt: fechaActual } }]}
+    const agrupaciones = await Suscripcion.find(query).select('agrupacion')         
+    .exec();
+    console.log("agrupaciones", agrupaciones)
+    listIdAgrupaciones = agrupaciones.map(x=>{ return x.agrupacion})
+    console.log("agrupaciones lista id", listIdAgrupaciones)
+    if(arte && nombre) {
+     
+        actividades = await Actividad.find( 
+            {nombre: { $regex: nombre }, arte: { $regex: arte } , estado:estado}).select('_id').exec();
+            listIdActividad =  actividades.map(x=>  { return x._id} )
+            const servicios =  await Servicio.find({$and:[{actividad: {$in: listIdActividad}},  {agrupacion: {$in: listIdAgrupaciones}}]})
+           //const servicios =  await Servicio.find(  {agrupacion: {$in: listIdAgrupaciones}})
+      //      console.log("servicios", servicios)
+        res.json({
+            ok: true,
+            servicios
+        });
+    }else if(nombre){
+        actividades = await Actividad.find( 
+            {nombre: { $regex: nombre },  estado:estado}).select('_id').exec();
+            listIdActividad =  actividades.map(x=>  { return x._id} )
+            const servicios =  await Servicio.find({$and:[{actividad: {$in: listIdActividad}},  {agrupacion: {$in: listIdAgrupaciones}}]})
+        res.json({
+            ok: true,
+            servicios
+        });
+    }else if(arte) {
+        actividades = await Actividad.find( 
+            {arte:  { $regex: arte } ,  estado:estado}).select('_id').exec();
+            listIdActividad =  actividades.map(x=>  { return x._id} )
+            const servicios =  await Servicio.find({$and:[{actividad: {$in: listIdActividad}},  {agrupacion: {$in: listIdAgrupaciones}}]})
+    
+        res.json({
+            ok: true,
+            servicios
+        });
+    } else {
+        actividades = await Actividad.find( 
+            { estado:estado }).select('_id').exec();
+            listIdActividad =  actividades.map(x=>  { return x._id} )
+            const servicios =  await Servicio.find({$and:[{actividad: {$in: listIdActividad}},  {agrupacion: {$in: listIdAgrupaciones}}]})
+        res.json({
+            ok: true,
+            servicios
+        });
+    }
+    
+    
+
+
+})
+
+
+*/
+export default agendaRoutes;
+
